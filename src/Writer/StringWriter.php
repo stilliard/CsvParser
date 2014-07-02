6cade036ce9c93838d2e9a2dfaaf7129c6f3dbfe
Andrew Stilliard <andrew.stilliard@gmail.com>
2014-07-02 11:27:02 +0100

    Fixed php 5.3 compatability in string writer class

--- src/Writer/StringWriter.php
+++ src/Writer/StringWriter.php
@@ -8,11 +8,11 @@ class StringWriter implements WriterInterface
     {
         $data = $csv->getData();
 
-        $output = [
+        $output = array(
             implode($parser->fieldDelimiter, array_map(function ($value) use ($parser) {
                 return $parser->fieldEnclosure . $value . $parser->fieldEnclosure;
             }, array_keys($data[1])))
-        ];
+        );
 
         foreach ($data as $line) {
             $output[] = implode($parser->fieldDelimiter, array_map(function ($value) use ($parser) {
