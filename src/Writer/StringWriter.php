2eb467b4aad116380e98b84d466a16d0081f01d1
Andrew Stilliard <andrew.stilliard@gmail.com>
2014-07-02 11:29:05 +0100

    Fixed finding first array for keys

--- src/Writer/StringWriter.php
+++ src/Writer/StringWriter.php
@@ -11,7 +11,7 @@ class StringWriter implements WriterInterface
         $output = array(
             implode($parser->fieldDelimiter, array_map(function ($value) use ($parser) {
                 return $parser->fieldEnclosure . $value . $parser->fieldEnclosure;
-            }, array_keys($data[1])))
+            }, array_keys(current($data))))
         );
 
         foreach ($data as $line) {
