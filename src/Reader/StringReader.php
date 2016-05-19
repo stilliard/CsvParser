f6b90831ba53ad578e9e9731f887d9c73b4713dd
Andrew Stilliard <andrew.stilliard@gmail.com>
2016-05-19 21:17:40 +0100

    Re-worked string reader to handle line delim detection better, especially when the new-line or line-delim is also inside fields

--- src/Reader/StringReader.php
+++ src/Reader/StringReader.php
@@ -7,29 +7,40 @@ class StringReader implements ReaderInterface
     public static function read(\CsvParser\Parser $parser, $string)
     {
         $data = array();
-        $headings = array();
 
-        // split lines
-        // regex from here: https://bugs.php.net/bug.php?id=55763
-        $lines = preg_split('/[\r\n]{1,2}(?=(?:[^'.$parser->fieldEnclosure.']*'.$parser->fieldEnclosure.'[^'.$parser->fieldEnclosure.']*'.$parser->fieldEnclosure.')*(?![^'.$parser->fieldEnclosure.']*'.$parser->fieldEnclosure.'))/', $string);
+        // shorten some vars for use later
+        $d = $parser->fieldDelimiter;
+        $e = $parser->fieldEnclosure;
+        $l = $parser->lineDelimiter;
 
-        foreach ($lines as $i => $line) {
-            if ($line==='') {
-                continue; // blank line...
-            }
-            $fields = str_getcsv($line, $parser->fieldDelimiter, $parser->fieldEnclosure);
-            if ($i===0) {
-                // first line, column headings
-                $headings = $fields;
-            }
-            else {
-                $data[$i-1] = array();
+        // get headings and body
+        list($headings, $body) = explode($l, $string, 2);
+        // format array of headings/keys
+        $headings = str_getcsv($headings, $d, $e);
+        $numDelims = count($headings) -1; // number of field delims to find per line
+
+        // tricky bit of regex, optionally matching the text enclosure (ref as \2 after first match),
+        // and catches any content inside this enclosure, even if that would be the field or line delim
+        // then repeating this match followed by the field delim for the number of columns we need (minus 1) and then the match again this time without the field delim
+        // then splits the lines only when not in the enclosure
+        preg_match_all('/(('. $e .')?[\s\S]*?\2?'. $d .'){'. $numDelims .'}\2?[\s\S]*?\2?('. $l .'|$)/', $body, $lines);
+
+        // any lines found? loop them
+        if ( ! empty($lines) && ! empty($lines[0])) {
+            foreach ($lines[0] as $i => $line) {
+                if ($line==='') {
+                    continue; // blank line...
+                }
+                $fields = str_getcsv($line, $d, $e);
+                $data[$i] = array();
+                // loop the headings to map to columns
                 foreach ($headings as $j => $heading) {
                     $field = isset($fields[$j]) ? $fields[$j] : '';
-                    $data[$i-1][$heading] = $field;
+                    $data[$i][$heading] = $field;
                 }
             }
         }
+
         return new \CsvParser\Csv($data);
     }
 }
