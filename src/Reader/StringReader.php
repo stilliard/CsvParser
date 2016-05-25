99dd3c9241dd8ad7d644458625affb395dc5013c
Andrew Stilliard <andrew.stilliard@gmail.com>
2016-05-25 14:01:39 +0100

    Now reading mac excel files better as these use carriage returns instead of line feeds, so if default line feeds set as lines then we will also accept carriage returns only when reading the file :)

--- src/Reader/StringReader.php
+++ src/Reader/StringReader.php
@@ -13,8 +13,9 @@ class StringReader implements ReaderInterface
         $e = $parser->fieldEnclosure;
         $l = $parser->lineDelimiter;
 
-        // get headings and body
-        list($headings, $body) = explode($l, $string, 2);
+        // get headings and body (if \n line feeds, also support reading on carriage returns)
+        list($headings, $body) = $l=="\n" ? preg_split('/[\n\r]/', $string, 2) : explode($l, $string, 2);
+
         // format array of headings/keys
         $headings = str_getcsv($headings, $d, $e);
         $numDelims = count($headings) -1; // number of field delims to find per line
@@ -23,7 +24,7 @@ class StringReader implements ReaderInterface
         // and catches any content inside this enclosure, even if that would be the field or line delim
         // then repeating this match followed by the field delim for the number of columns we need (minus 1) and then the match again this time without the field delim
         // then splits the lines only when not in the enclosure
-        preg_match_all('/(('. $e .')?[\s\S]*?\2?'. $d .'){'. $numDelims .'}\2?[\s\S]*?\2?('. $l .'|$)/', $body, $lines);
+        preg_match_all('/(('. $e .')?[\s\S]*?\2?'. $d .'){'. $numDelims .'}\2?[\s\S]*?\2?('. ($l=="\n" ? '\n|\r' : $l) .'|$)/', $body, $lines);
 
         // any lines found? loop them
         if ( ! empty($lines) && ! empty($lines[0])) {
