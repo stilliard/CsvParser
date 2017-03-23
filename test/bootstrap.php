8bf59fd5bd4887ab1a5adf479c3b73f5bbbb45ef
Andrew Stilliard <andrew.stilliard@gmail.com>
2017-03-23 00:50:43 +0000

    Fixing travis CI phpunit version upgrade

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,3 +2,8 @@
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
