9f83c65e1184d80881a9899945b99b24b23b2dc4
Andrew Stilliard <andrew.stilliard@gmail.com>
2017-03-23 00:54:11 +0000

    wrong way round on alias

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,7 +3,7 @@
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 // backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') &&
-    class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+if (!class_exists('\PHPUnit_Framework_TestCase') &&
+    class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 }
