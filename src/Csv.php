82d13afc228034bc5e7b06f3c1f4390085ebcdbd
Andrew Stilliard <andrew.stilliard@gmail.com>
2019-05-23 16:49:39 +0100

    feat: new methods for reKey(?keys) getKeys() and first() on a csv

--- src/Csv.php
+++ src/Csv.php
@@ -21,6 +21,31 @@ class Csv
         return count($this->data);
     }
 
+    public function first()
+    {
+        return current($this->data);
+    }
+
+    public function getKeys()
+    {
+        return array_keys($this->first());
+    }
+
+    /**
+     * ->reKey allows you to reorganise the keys if they may be out of order as when we write to csv we do it in the order given
+     * e.g. if you build up an array, adding new columns/keys as you go down the multi levels, so long as you push them into the keys passed here or leave blank and use the first row
+     */
+    public function reKey($keys = null)
+    {
+        if (! $keys) {
+            $keys = $this->getKeys();
+        }
+        $template = array_map(function () { return ''; }, array_flip($keys));
+        $this->mapRows(function ($row) use ($template) {
+            return array_replace($template, $row);
+        });
+    }
+
     public function appendRow($row)
     {
         array_unshift($this->data, $row);
