0f817ff9bad8642c811b56c89b57242a12ae63c3
Andrew Stilliard <andrew.stilliard@gmail.com>
2022-11-16 22:14:04 +0000

    feat(string): allow for numeric arrays to not use a header line

--- src/Writer/StringWriter.php
+++ src/Writer/StringWriter.php
@@ -4,16 +4,24 @@ namespace CsvParser\Writer;
 
 class StringWriter implements WriterInterface
 {
+    protected static function isAssoc(array $array)
+    {
+        return array_keys($array) !== range(0, count($array) - 1);
+    }
+
     public static function write(\CsvParser\Parser $parser, \CsvParser\Csv $csv)
     {
         $data = $csv->getData();
 
         if ($data && !empty($data)) {
-            $output = array(
-                implode($parser->fieldDelimiter, array_map(function ($value) use ($parser) {
+            $firstLine = current($data);
+            $output = [];
+            // only write header if the first line is an associative array
+            if (self::isAssoc($firstLine)) {
+                $output[] = implode($parser->fieldDelimiter, array_map(function ($value) use ($parser) {
                     return $parser->fieldEnclosure . str_replace($parser->fieldEnclosure, $parser->fieldEnclosure.$parser->fieldEnclosure, $value) . $parser->fieldEnclosure;
-                }, array_keys(current($data))))
-            );
+                }, array_keys($firstLine)));
+            }
 
             foreach ($data as $line) {
                 $output[] = implode($parser->fieldDelimiter, array_map(function ($value) use ($parser) {
--- test/Writer/StringWriterTest.php
+++ test/Writer/StringWriterTest.php
@@ -10,10 +10,25 @@ class StringWriterTest extends \PHPUnit_Framework_TestCase
         $this->parser = new Parser(',', '');
     }
 
-    public function testWrite()
+    public function testWriteAssoc()
     {
         $input = array(array( 'a' => 1, 'b' => 2, 'c' => 44 ));
         $expected = "a,b,c\n1,2,44";
+        $actual = StringWriter::write($this->parser, $this->parser->fromArray($input));
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testWriteNumeric()
+    {
+        $input = array(array( 1, 2, 44 ));
+        // numeric arrays should not have a header
+        $expected = "1,2,44";
+        $actual = StringWriter::write($this->parser, $this->parser->fromArray($input));
+        $this->assertSame($expected, $actual);
+
+        $input = array(array( '1' => 1, '2' => 2, '3' => 44 ));
+        // however if they keys are strings, even if numbers, they should have a header
+        $expected = "1,2,3\n1,2,44";
         $actual = StringWriter::write($this->parser, $this->parser->fromArray($input));
         $this->assertSame($expected, $actual);
     }
